3e65556b2299f8bcefd489230e1ca9f3a5e091fa
awongCM <awong.cm@gmail.com>
2021-10-14 14:38:12 +1100

    added bodyparser middlewares

--- auth/src/server.js
+++ auth/src/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.raw);
 
 module.exports = app;

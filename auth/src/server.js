2acb4f7d2070f679f834cd3e3779401d1f4e9e11
awongCM <awong.cm@gmail.com>
2021-10-15 13:07:07 +1100

    added todo notes

--- auth/src/server.js
+++ auth/src/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// TODO - EJS module setup
+// import { authRoute } from "./routes";
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.raw);
+// app.use(authRoute);
 
 module.exports = app;

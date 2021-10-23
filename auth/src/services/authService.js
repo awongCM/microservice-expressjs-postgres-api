41d0f034a6d7e3ee452750e9e64d1d2591323789
awongCM <awong.cm@gmail.com>
2021-10-23 14:22:30 +1100

    added todo notes here

--- auth/src/services/authService.js
+++ auth/src/services/authService.js
@@ -1,1 +1,7 @@
-// TODO
+// TODO - complete rest of implementation
+import { getJWTToken, getSaltHashPassword, verifyPassword } from "../utils";
+import {
+  CustomerAlreadyExistsException,
+  WrongLoginInfoException,
+} from "../exceptions";
+import { Customer } from "../db/models";
